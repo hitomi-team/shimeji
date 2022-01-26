427aa40077b3b7bb2553b7f2198eb5489a874333
Tony <26317155+harubaru@users.noreply.github.com>
2022-01-26 15:25:08 -0700

    Add docstrings for ModelProviders and Shimeji

--- shimeji/model_provider.py
+++ shimeji/model_provider.py
@@ -2,7 +2,14 @@ import requests
 import json
 
 class ModelProvider:
+    """Abstract class for model providers that provide access to generative AI models.
+    """
     def __init__(self, endpoint_url: str, **kwargs):
+        """Constructor for ModelProvider.
+
+        :param endpoint_url: The URL of the endpoint.
+        :type endpoint_url: str
+        """
         self.endpoint_url = endpoint_url
         self.kwargs = kwargs
         if 'args' not in kwargs:
@@ -10,24 +17,58 @@ class ModelProvider:
         self.auth()
     
     def auth(self):
-        pass
+        """Authenticate with the ModelProvider's endpoint.
+
+        :raises NotImplementedError: If the authentication method is not implemented.
+        """
+        raise NotImplementedError('auth method is required')
 
     def generate(self, args):
-        pass
+        """Generate a response from the ModelProvider's endpoint.
+        
+        :param args: The arguments to pass to the endpoint.
+        :type args: dict
+        :raises NotImplementedError: If the generate method is not implemented.
+        """
+        raise NotImplementedError('generate method is required')
 
     def should_respond(self, context, name):
-        pass
+        """Determine if the ModelProvider predicts that the name should respond to the given context.
+
+        :param context: The context to use.
+        :type context: str
+        :param name: The name to check.
+        :type name: str
+        :raises NotImplementedError: If the should_respond method is not implemented.
+        """
+        raise NotImplementedError('should_respond method is required')
 
     def response(self, context):
-        pass
+        """Generate a response from the ModelProvider's endpoint.
+            
+        :param context: The context to use.
+        :type context: str
+        :raises NotImplementedError: If the response method is not implemented.
+        """
+        raise NotImplementedError('response method is required')
 
 class Sukima_ModelProvider(ModelProvider):
     def __init__(self, endpoint_url: str, **kwargs):
+        """Constructor for Sukima_ModelProvider.
+
+        :param endpoint_url: The URL for the Sukima endpoint.
+        :type endpoint_url: str
+        """
+
         super().__init__(endpoint_url, **kwargs)
         self.auth()
     
     def auth(self):
-        # check if username and password are not in kwargs, raise exception if token is not in kwargs too
+        """Authenticate with the Sukima endpoint.
+
+        :raises Exception: If the authentication fails.
+        """
+
         if 'username' not in self.kwargs and 'password' not in self.kwargs:
             raise Exception('username, password, and or token are not in kwargs')
         
@@ -41,6 +82,14 @@ class Sukima_ModelProvider(ModelProvider):
             raise Exception(f'Could not authenticate with Sukima. Error: {r.text}')
         
     def generate(self, args):
+        """Generate a response from the Sukima endpoint.
+        
+        :param args: The arguments to pass to the endpoint.
+        :type args: dict
+        :return: The response from the endpoint.
+        :rtype: str
+        :raises Exception: If the request fails.
+        """
         try:
             r = requests.post(f'{self.endpoint_url}/api/v1/models/generate', data=json.dumps(args), headers={'Authorization': f'Bearer {self.token}'}, timeout=30.0)
         except Exception as e:
@@ -51,6 +100,15 @@ class Sukima_ModelProvider(ModelProvider):
             raise Exception(f'Could not generate text with Sukima. Error: {r.json()}')
     
     def should_respond(self, context, name):
+        """Determine if the Sukima endpoint predicts that the name should respond to the given context.
+
+        :param context: The context to use.
+        :type context: str
+        :param name: The name to check.
+        :type name: str
+        :return: Whether or not the name should respond to the given context.
+        :rtype: bool
+        """
         args = self.kwargs['args'] # get default args
         args['prompt'] = context
         args['gen_args']['eos_token_id'] = 25
@@ -63,6 +121,13 @@ class Sukima_ModelProvider(ModelProvider):
             return False
 
     def response(self, context):
+        """Generate a response from the Sukima endpoint.
+
+        :param context: The context to use.
+        :type context: str
+        :return: The response from the endpoint.
+        :rtype: str
+        """
         args = self.kwargs['args']
         args['prompt'] = context
         args['gen_args']['eos_token_id'] = 198
--- shimeji/shimeji.py
+++ shimeji/shimeji.py
@@ -1,12 +1,16 @@
 import logging
-from shimeji.model_provider import ModelProvider
-
-# model_provider - provides an API or an interface to inference a GPT model
-# preprocessors - used to preprocess the input before inference
-# postprocessors - used to postprocess the output after inference
+import shimeji.model_provider
 
 class ChatBot:
     def __init__(self, name, model_provider, **kwargs):
+        """Constructor for ChatBot.
+
+        :param name: The name of the chatbot.
+        :type name: str
+        :param model_provider: The model provider to use.
+        :type model_provider: ModelProvider
+        """
+
         self.name = name
         self.model_provider = model_provider
 
@@ -14,9 +18,18 @@ class ChatBot:
         self.postprocessors = kwargs.get('postprocessors', [])
 
         self.conversation_chain = []
-    
-    # go through preprocessor but forgo postprocessing stage
+
     def should_respond(self, text, push_chain):
+        """Determine if the chatbot should respond to the given text or conversation chain.
+
+        :param text: The response text.
+        :type text: str
+        :param push_chain: Whether to push the response to the conversation chain.
+        :type push_chain: bool
+        :return: Whether the chatbot should respond.
+        :rtype: bool
+        """
+
         if push_chain:
             self.conversation_chain.append(text)
         if self.conversation_chain:
@@ -29,6 +42,14 @@ class ChatBot:
         return self.model_provider.should_respond(text, self.name)
 
     def respond(self, text, push_chain):
+        """Respond to the given text or conversation chain.
+
+        :param text: The response text.
+        :type text: str
+        :param push_chain: Whether to push the response to the conversation chain.
+        :type push_chain: bool
+        """
+
         if push_chain:
             self.conversation_chain.append(text)
         if self.conversation_chain:
@@ -50,6 +71,16 @@ class ChatBot:
         return response
 
     def conditional_response(self, text, push_chain=True):
+        """Respond to the given text or conversation chain if the chatbot should respond.
+
+        :param text: The response text.
+        :type text: str
+        :param push_chain: Whether to push the response to the conversation chain.
+        :type push_chain: bool
+        :return: Whether the chatbot should respond.
+        :rtype: bool
+        """
+
         if self.should_respond(text, push_chain):
             return self.respond(text, False)
         else:
