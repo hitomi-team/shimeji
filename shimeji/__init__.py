2e79b011697115895e09baf99d16f44ba00c551e
Tony <26317155+harubaru@users.noreply.github.com>
2022-01-26 15:17:45 -0700

    Update __init__.py

--- shimeji/__init__.py
+++ shimeji/__init__.py
@@ -1,3 +1,13 @@
-from .shimeji import ChatBot
+__title__ = 'shimeji'
+__version__ = '0.1.0'
+__author__ = 'hitomi-team'
+__license__ = 'BSD 2-Clause License'
+__copyright__ = 'Copyright 2022 hitomi-team'
 
-__all__ = ('ChatBot',)
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+from .shimeji import *
+from .model_provider import *
+from .preprocessor import *
+from .postprocessor import *
+from .util import *
